0ed96ffde32f3d3d85042ebf8f61d1219b932d36
Jamal <mdjamalansari939@gmail.com>
2023-06-16 19:55:58 +0530

    first commit

---
+++ gitcommand.js
@@ -0,0 +1,11 @@
+                           
+                           /*There are some code that is use in git */
+
+
+// ls => for open list
+// git status => for checking staging area, the file is add . or not
+// git init => for initalizing
+// git add => for add file in staging area,
+// git commit -m "First Commit" => that is use for add commit that can help you for maintain when u commit this
+// git branch -M main => This is use for change branch
+
